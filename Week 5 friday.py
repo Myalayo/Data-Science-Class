a55fb22c0e3e9179b464abec30961f0418d12f76
unknown <adijatatanda123@gmail.com>
2024-06-14 14:10:14 +0100

    Loops Functions

---
+++ Week 5 friday.py
@@ -0,0 +1,64 @@
+LG_in_kwara = ["ilorin west", "ekiti", "oke-ero", "ifelodun","barutee","asa", "ilorin South", "Kaima"]
+
+#Find Lg in the even position
+
+length = len(LG_in_kwara) # This is called a function
+def double(x):
+    print (x * 2)
+# for i in range((5)):
+#     print(LG_in_kwara[i])
+
+# for i in range (length):
+#     print(LG_in_kwara[i])
+#     print(LG_in_kwara[1:9:2])
+
+# for i in range (1,length,2):
+#     print(LG_in_kwara[i])
+
+# for i in range (length):
+#     if i % 2 == 1:
+#         print(LG_in_kwara[i])
+
+#Or this method
+# for i in range (length):
+#     if (i+1) % 2 == 0:
+#         print(LG_in_kwara[i])
+
+# for i in range (length):
+#     if (i+1) % 2 == 1:
+#         print(LG_in_kwara[i])
+
+# for i in range (length):
+#     if i % 2 == 0:
+#        print(LG_in_kwara[i]) 
+
+#print all the LGA that start with vowels letter
+
+# vowels = "aeiou"
+# vowelsArray = [a, e, i, o, u]
+
+# for char in vowels:
+#     print(char)
+
+# for i in range(length):
+#     if LG_in_kwara[i][0] in vowels:
+#         print(LG_in_kwara[i])   
+
+# for lg in LG_in_kwara:
+#     if lg[0] in vowels:
+#         print(lg) 
+
+double(7)
+double(3)
+double(15)
+
+# for i in range (length):
+#     LG_in_kwara[i] =  LG_in_kwara[i]  + ' state'
+#     print(LG_in_kwara[i])
+  
+# print(LG_in_kwara)
+
+for i in range (length):
+    LG_in_kwara[i] =  LG_in_kwara[i]  + ' LG'
+    print(LG_in_kwara[i])
+  
---
+++ Week 5.py
@@ -0,0 +1,78 @@
+# #For Loop Function = To iterate; Iterable. the code always run from top to bottom 
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# #this simply mean instead of wriitng print (1) to the number you want to print, you can simply do it this way 
+# # your i can be named as anything e.g num
+# name ="Alayo" 
+# num = 321
+# for num in range(6):
+#     print(num) #NB it start to count from 0, so if you want it to show number 6 add +1 to num. i.e (num)
+# for num in range (1,13):
+#     print(num)
+#     print(name.upper()) #Method 
+#     #print(num.upper())
+#     print(type(num)) #function
+#     print(type(num))
+# for num in range (1,10): #end range is exclusive, start range is inclusive, which means it won't add d end number
+#     print(num)
+# for num in range (2,12):
+#     print(num)
+# for num in range (5,12,3): #1 1 (Start range), 2(end range), 3(step), the 3 are optional only one is needed 
+#     print(num)
+# for num in range (13,2, -1):
+#     print(num)
+# for num in range (4):
+#     print(num)
+#     print ("cool")
+#     print ("you are good")
+# print("you are good")
+# for num in range (3,37,3):
+#     print(num)
+# name_list = ["damilola", "Aisha", "Khadija", "Fatima"]
+# for name in name_list:
+#     print(name)
+
+# #OR you can call it one by one
+# print(name_list[0])
+# print(name_list[1])
+# print(name_list[2])
+# print(name_list[3])
+# list_length = len(name_list)
+# print(list_length)
+# for i in range (list_length):
+#     print(i)
+#     print(name_list[i])
+# for i in range (list_length):
+#     print(name_list)
+#     if i == 2: #THis simply mean I am changing Khadija to klaz. That is from this ["damilola", "Aisha", "Khadija", "Fatima"] 
+#         name_list[i] = "Klaz"
+#         print(name_list)
+# name = "president Alayo"
+
+# for letter in name:
+#     print(letter)
+#     for i in range(len(name)):
+#         print(name[i])
+
+# LG_in_kwara = ["ilorin west", "ekiti", "oke-ero", "ifelodun","barutee","asa"]
+
+# for LG in LG_in_kwara:
+#     print(LG[-1])
+
+# Country = "Nigeria"
+# # for letter in Country:
+# #     print(letter)
+# print(len(Country))
+
+Sentence = "Nigeria is my Country"
+# Mine = Sentence.split()
+# print (Mine)
+for name in Sentence:
+    print(Sentence)
+
+
+
